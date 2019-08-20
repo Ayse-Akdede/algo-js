954ab7b4fb55589e409d1fbc8e65a1dd00c491ad
Ayse Akdede <ayseazize.akdede@gmail.com>
2019-08-20 15:06:02 +0200

    ajout de exo 1.2

---
+++ 1.2.js
@@ -0,0 +1,6 @@
+const readlineSync = require("readline-sync");
+let name = 'Claude'
+let firstname = 'Jean'
+let city = 'Amsterdam'
+
+console.log('Your name is ' + firstname + ' ' + name + ' you live in ' + city)
